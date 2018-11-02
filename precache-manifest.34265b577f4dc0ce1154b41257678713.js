f0779d7bae1e3b6217ca5274d0b3b89c9fbc9706
Bruno Wowk <bruno.wowk@gmail.com>
2018-11-02 13:23:34 -0300

    Updates

--- precache-manifest.34265b577f4dc0ce1154b41257678713.js
+++ precache-manifest.34265b577f4dc0ce1154b41257678713.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/maze-rats-lab/static/media/icons.faff9214.woff"
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/maze-rats-lab/static/media/brand-icons.e8c322de.woff2"
   },
   {
-    "revision": "a0a94d3092551eb526d1",
+    "revision": "daa138a320c3b43cfe2c",
     "url": "/maze-rats-lab/static/css/main.30a3997e.chunk.css"
   },
   {
@@ -44,12 +44,12 @@ self.__precacheManifest = [
     "url": "/maze-rats-lab/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "a0a94d3092551eb526d1",
-    "url": "/maze-rats-lab/static/js/main.a0a94d30.chunk.js"
+    "revision": "daa138a320c3b43cfe2c",
+    "url": "/maze-rats-lab/static/js/main.daa138a3.chunk.js"
   },
   {
-    "revision": "e8c322de9658cbeb8a774b6624167c2c",
-    "url": "/maze-rats-lab/static/media/brand-icons.e8c322de.woff2"
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/maze-rats-lab/static/media/icons.faff9214.woff"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
@@ -60,20 +60,20 @@ self.__precacheManifest = [
     "url": "/maze-rats-lab/static/media/flags.9c74e172.png"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/maze-rats-lab/static/media/icons.b87b9ba5.ttf"
-  },
-  {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/maze-rats-lab/static/media/brand-icons.13db00b7.eot"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/maze-rats-lab/static/media/icons.8e3c7f55.eot"
   },
   {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/maze-rats-lab/static/media/icons.b87b9ba5.ttf"
+  },
+  {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "/maze-rats-lab/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/maze-rats-lab/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
@@ -88,7 +88,7 @@ self.__precacheManifest = [
     "url": "/maze-rats-lab/static/css/1.ac373dad.chunk.css"
   },
   {
-    "revision": "e3ce4b14a0bcc79c4fa605f09698468a",
+    "revision": "b59048a56e298fab14801a8f29023d8a",
     "url": "/maze-rats-lab/index.html"
   }
 ];
