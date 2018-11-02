fe62f8e8928d07abb4b9ae41e0de53c821801b18
Bruno Wowk <bruno.wowk@gmail.com>
2018-11-02 01:08:06 -0300

    Fixes language switching

--- src/components/LangSelector/index.js
+++ src/components/LangSelector/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { Select } from 'semantic-ui-react'
 
-const LangSelector = withRouter(({history}) => {
+const LangSelector = withRouter(({history, location}) => {
     let countryOptions = [
         { key: 'ptbr', value: 'ptbr', flag: 'br', text: 'Brazillian Portuguese' },
         { key: 'en', value: 'en', flag: 'us', text: 'English' }
     ]
     return (
-        <Select style={{ fontSize: 10, width:"200px" }} onChange={(e,{k, value}) => history.push('/?lang='+value)} placeholder='Change language' options={countryOptions} />
+        <Select style={{ fontSize: 10, width:"200px" }} onChange={(e,{k, value}) => history.push(location.pathname+'?lang='+value)} placeholder='Change language' options={countryOptions} />
     )
 })
 
