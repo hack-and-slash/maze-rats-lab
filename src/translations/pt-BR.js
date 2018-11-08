c9e9bbc5d9084ed772c8fea7a75bea8cd0b9603f
Bruno Wowk <bruno.wowk@gmail.com>
2018-11-07 22:45:09 -0200

    Sorts translation resource

--- src/translations/pt-BR.js
+++ src/translations/pt-BR.js
@@ -1,10 +1,11 @@
+/*eslint sort-keys: "error"*/
 export default {
+    "Brazillian Portuguese": "Português Brasileiro",
+    "Change language": "Trocar idioma",
     "Character generator": "Gerador de personagens",
+    "English": "Inglês",
     "Spell generator": "Gerador de magias",
     "The <1>lazy</1> pragmatic player toolbox": {
         "": "A ferramenta do jogador <1>preguiçoso</1> pragmático."
     },
-    "Brazillian Portuguese": "Português Brasileiro",
-    "English": "Inglês",
-    "Change language": "Trocar idioma"
 }
